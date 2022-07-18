de37ed8131bd0c82e363b40180f8a2b34abdb72d
The-metre <hauddar@gmail.com>
2022-07-18 22:01:37 +0300

    added models to admin page

--- sandbox/chords/admin.py
+++ sandbox/chords/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Artist, Track, Chords, Watchlist
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
@@ -11,4 +11,9 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['email', 'username', ]
 
+# register all existing models
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Artist)
+admin.site.register(Track)
+admin.site.register(Chords)
+admin.site.register(Watchlist)
--- sandbox/chords/models.py
+++ sandbox/chords/models.py
@@ -4,3 +4,34 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     pass
 
+class Artist(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    description = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Artist: {self.name}."
+
+class Track(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    text = models.CharField(max_length=2000, blank=True)
+    description = models.CharField(max_length=1000, blank=True)
+    creator = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='created_track')
+
+    def __str__(self):
+        return f'Track: {self.name} by {self.creator}'
+
+class Chords(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    symbol = models.CharField(max_length=255, blank=False)
+    steps = models.CharField(max_length=255, blank=False)
+    notes = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return f'{self.name} chord'
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='added_to_watchlist')
+    track = models.ForeignKey(Track, on_delete=models.CASCADE, related_name='added_to_watchlist')
+
+    def __str__(self):
+        return f'{self.user} added {self.track} to watchlist'
